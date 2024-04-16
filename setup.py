7fb00b45d4b09823e6cb5f8e90589fbe15261d4c
michalskibinski109 <michalskibinski109@gmail.com>
2024-04-16 20:20:50 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __author__ = "Michał Skibiński"
 
 this_directory = Path(__file__).parent
